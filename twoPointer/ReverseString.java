14a343021eda129e1f311373e76a7aa3b51da4ff
Ajay Kumar <abharoja@gmail.com>
2026-04-01 18:30:49 +0530

    two pointer solutions

---
+++ twoPointer/ReverseString.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class ReverseString {
+    public static void reverseString(char[] str) {
+        int left = 0;
+        int right = str.length - 1;
+
+        while (left < right) {
+            char temp = str[left];
+            str[left] = str[right];
+            str[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args) {
+        char[] str = {'h','e','l','l','o'};
+        reverseString(str);
+        System.out.println(Arrays.toString(str));
+    }
+}
